be3822a470ef7654ba99e8550737b0230095f5d9
MaNeiNei09 <35565754+MaNeiNei09@users.noreply.github.com>
2018-09-16 16:42:28 +0200

    V0.1.1
    V0.1.1

--- TestGUI.py
+++ TestGUI.py
@@ -9,32 +9,35 @@
 import tkinter as tk
 
 window = tk.Tk()
-window.title('ZSmart')
+window.title('我是无聊的小胖纸')
 window.geometry('200x100')
 
 var = tk.StringVar()
 
 l = tk.Label(window,
              textvar=var,
-             bg='blue',
+             bg='white',
              font=('Arial',12),
              width=15,
              height=2)
 l.pack()
 
-on_hit = False
+on_hit = 0
 
 def hit_me():
     global on_hit
-    if on_hit == False:
-        on_hit = True
-        var.set('Hello,hit me')
+    if on_hit == 0:
+        on_hit = 1
+        var.set('Hello,媳妇')
+    elif on_hit == 1:
+        on_hit = 2
+        var.set('我爱你')
     else:
-        on_hit = False
-        var.set('OMG')
+        var.set('你最美')
+
 
 b = tk.Button(window,
-              text='Log in',
+              text='点点我',
               width=5,
               height=5,
               command=hit_me)
