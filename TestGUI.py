18b397786603b5bcc3484c93818d4960481a499b
MaNeiNei09 <35565754+MaNeiNei09@users.noreply.github.com>
2018-09-16 15:48:05 +0200

    Tkinter
    Tkinter

---
+++ TestGUI.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*
+
+'''
+# Pyhton GUI V0.1
+# date = 2018.09.16
+# author = zbh
+'''
+import tkinter as tk
+
+window = tk.Tk()
+window.title('ZSmart')
+window.geometry('200x100')
+
+var = tk.StringVar()
+
+l = tk.Label(window,
+             textvar=var,
+             bg='blue',
+             font=('Arial',12),
+             width=15,
+             height=2)
+l.pack()
+
+on_hit = False
+
+def hit_me():
+    global on_hit
+    if on_hit == False:
+        on_hit = True
+        var.set('Hello,hit me')
+    else:
+        on_hit = False
+        var.set('OMG')
+
+b = tk.Button(window,
+              text='Log in',
+              width=5,
+              height=5,
+              command=hit_me)
+b.pack()
+
+window.mainloop()
